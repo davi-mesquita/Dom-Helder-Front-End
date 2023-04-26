6710fbf90885ab34e00c84129dadcf8c385ccdb4
Davi Mesquita da Fonseca <davimesquita9@gmail.com>
2023-04-26 12:28:24 -0300

    algumas coisas feitas

--- Algoritimos/C06X13.java
+++ Algoritimos/C06X13.java
@@ -2,6 +2,45 @@ import javax.swing.JOptionPane;
 public class C06X13 {
     //Autor: Davi Mesquita da Fonseca
     public static void main(String[] args) {
-        
+        String hi, mi, hf, mf;
+        int hi1, mi1, hf1, mf1;
+        hi = JOptionPane.showInputDialog
+        (
+            null,
+            "Hora inicial: ",
+            "Aula de Java",
+            JOptionPane.QUESTION_MESSAGE
+        );
+        hi1 = Integer.parseInt(hi);
+        mi = JOptionPane.showInputDialog
+        (
+            null,
+            "Minuto inicial: ",
+            "Aula de Java",
+            JOptionPane.QUESTION_MESSAGE
+        );
+        mi1 = Integer.parseInt(mi);
+        hf = JOptionPane.showInputDialog
+        (
+            null,    
+            "Hora final: ",
+            "Aula de Java",
+            JOptionPane.QUESTION_MESSAGE
+        );
+        hf1 = Integer.parseInt(hf);
+        mf = JOptionPane.showInputDialog
+        (
+            null,    
+            "Minuto final: ",
+            "Aula de Java",
+            JOptionPane.QUESTION_MESSAGE
+        );
+        mf1 = Integer.parseInt(mf);
+        int t1 = hi1 * 60 + mi1;
+        int t2 = hf1 * 60 + mf1;
+        int t3 = t2 - t1;
+        int t4 = t3 / 60;
+        int t5 = t3 % 60;
+        JOptionPane.showMessageDialog(null, "O tempo total de duração foi de " + t4 + " horas e " + t5 + " minutos.");
     }
 }
--- Algoritimos/C06X17.java
+++ Algoritimos/C06X17.java
@@ -2,6 +2,98 @@ import javax.swing.JOptionPane;
 public class C06X17 {
     //Autor: Davi Mesquita da Fonseca
     public static void main(String[] args) {
+        String[] entrada = {"Sim","Não"};
+        int[] valor = new int[8];
+        valor[0] = JOptionPane.showOptionDialog(
+            null,
+            "Você tem curso técnico de programação",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[1] = JOptionPane.showOptionDialog(
+            null,
+            "Você tem curso superior de programação",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[2] = JOptionPane.showOptionDialog(
+            null,
+            "Você tem menos de 3 anos de experiência em programação",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[3] = JOptionPane.showOptionDialog(
+            null,
+            "Você se considera uma pessoa criativa",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[4] = JOptionPane.showOptionDialog(
+            null,
+            "Você prefere liderar a ser liderado",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[5] = JOptionPane.showOptionDialog(
+            null,
+            "Você prefere trabalhar sozinho a trabalhar em equipe",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[6] = JOptionPane.showOptionDialog(
+            null,
+            "Você é autodidata (Aprende Sozinho)",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[7] = JOptionPane.showOptionDialog(
+            null,
+            "Você aceitaria uma remuneração inicial de R$1500.00",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
+        valor[8] = JOptionPane.showOptionDialog(
+            null,
+            "Você aceitaria trabalhar em escritório da empresa dentro da grande BH",
+            "Entrevista de Java",
+            JOptionPane.INFORMATION_MESSAGE,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            entrada,
+            "Sim"
+        );
         
     }
 }
