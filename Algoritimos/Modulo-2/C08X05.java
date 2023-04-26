806232b564dbd2b4c51ad15d3213fc5f06b0dbb7
Davi Mesquita da Fonseca <77731429+davi-mesquita@users.noreply.github.com>
2023-04-25 21:43:45 -0300

    Organização de pastas e 2° modulo

---
+++ Algoritimos/Modulo-2/C08X05.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class C08X05 {
+    //Autor: Davi Mesquita da Fonseca
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        scanner.close();
+    }
+}
